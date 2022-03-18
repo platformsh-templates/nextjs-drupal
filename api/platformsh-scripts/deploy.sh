7e25d3c443d5a5c4beee365f91a4fce7a0016fbb
chadcarlson <chad.carlson@platform.sh>
2022-03-18 11:08:31 -0400

    Update script calls.

--- api/platformsh-scripts/deploy.sh
+++ api/platformsh-scripts/deploy.sh
@@ -45,16 +45,16 @@ else
     echo $UPDATED_DATA > $ENV_SETTINGS
 
     # 1. Install Drupal with default profile + creds.
-    ./$DRUPAL_SETUP/project/01-install-drupal.sh
+    $DRUPAL_SETUP/project/01-install-drupal.sh
 
     # 2. Enable modules.
-    ./$DRUPAL_SETUP/project/02-enable-modules.sh
+    $DRUPAL_SETUP/project/02-enable-modules.sh
 
     # 3. Create role and user.
-    ./$DRUPAL_SETUP/project/03-create-role-and-user.sh
+    $DRUPAL_SETUP/project/03-create-role-and-user.sh
 
     # 4. Configure content.
-    ./$DRUPAL_SETUP/project/04-configure-content.sh
+    $DRUPAL_SETUP/project/04-configure-content.sh
 
     # 5. Rebuild the cache.
     printf "    ✔ Rebuilding the cache.\n"
